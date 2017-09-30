215fad41bc66d3c1393f5c0d05cb6f7bda28ac8e
aadharbhatt <aadharbhatt@gmail.com>
2017-09-30 22:38:43 +0530

    test_working.py

--- test.py
+++ test.py
@@ -18,30 +18,11 @@ def sub():
     sub_topic = 'projects/{project}/subscriptions/{subscription}'.format(project=project_id,subscription=pubsub_subscription)
     subs = subscriber.subscribe(sub_topic)
 
-    """def callback(message):
+    def callback(message):
         print(message.data)
         message.ack()
 
-    subs.open(callback)"""
-
-    def receive_messages():
-        """Receives messages from a pull subscription."""
-        #subscriber = pubsub.SubscriberClient()
-        subscription_path = subscriber.subscription_path(project_id, pubsub_subscription)
-
-        def callback(message):
-            print('Received message: {}'.format(message))
-            message.ack()
-
-        subscriber.subscribe(subscription_path, callback=callback)
-
-        # The subscriber is non-blocking, so we must keep the main thread from
-        # exiting to allow it to process messages in the background.
-        print('Listening for messages on {}'.format(subscription_path))
-        while True:
-            time.sleep(60)
-
-
+    subs.open(callback)
 
 def variables():
     project_id = 'work-180507'
@@ -62,4 +43,6 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
 
